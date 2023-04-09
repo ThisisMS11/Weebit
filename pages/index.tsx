f92ae0278ba840edd44bf4db15ac05689ecacd8b
MohitSaini <mailtomohit2002@gmail.com>
2023-04-09 14:19:35 +0530

    Added Video Rendering Features

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,41 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React from 'react';
+import axios from 'axios';
 
-const inter = Inter({ subsets: ['latin'] })
+import VideoCard from '../components/VideoCard';
+import { BASE_URL } from '../utils';
+import { Video } from '../types';
+import NoResults from '../components/NoResults';
 
-export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world
-    </h1>
-  )
+interface IProps {
+  videos: Video[];
 }
+
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length 
+        ? videos?.map((video: Video) => (
+          <VideoCard post={video} isShowingOnHome key={video._id} />
+        )) 
+        : <NoResults text={`No Videos`} />}
+    </div>
+  );
+};
+
+export default Home;
+
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = await axios.get(`${BASE_URL}/api/post`);
+
+  if(topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  }
+  
+  return {
+    props: { videos: response.data },
+  };
+};
